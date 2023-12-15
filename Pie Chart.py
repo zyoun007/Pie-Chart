de87db1fd76e530684e085d02c782814f9e5a7a3
zyoun007 <146486881+zyoun007@users.noreply.github.com>
2023-12-14 18:12:44 -0600

    Update Pie Chart.py

--- Pie Chart.py
+++ Pie Chart.py
@@ -11,6 +11,7 @@ colors = ['b', 'r', 'm', 'c']
 def displayChart(title, label, values): 
     plt.pie(values, labels = slice_labels, colors = ['#008000', 'gold', 'indigo', 'black']) 
     #revised colors to green, gold, indigo, and black
+    #https://matplotlib.org/stable/users/explain/colors/colors.html and https://proclusacademy.com/blog/customize_matplotlib_piechart/ 
     #display the pie chart
     plt.show()
 
