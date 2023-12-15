dd3cad0c0662f5849cdbf304fabd4d9860edf2d6
zyoun007 <146486881+zyoun007@users.noreply.github.com>
2023-12-14 18:10:50 -0600

    Add files via upload

---
+++ Pie Chart.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+    #create a list of values
+valuesPie = [45, 24, 36, 10]
+    
+
+    #create pie chart from values
+slice_labels = ['Q1', 'Q2', 'Q3', 'Q4']
+titlePie = plt.title ('Sales Data')
+colors = ['b', 'r', 'm', 'c']
+def displayChart(title, label, values): 
+    plt.pie(values, labels = slice_labels, colors = ['#008000', 'gold', 'indigo', 'black']) 
+    #revised colors to green, gold, indigo, and black
+    #display the pie chart
+    plt.show()
+
+displayChart(titlePie, slice_labels, valuesPie)  
